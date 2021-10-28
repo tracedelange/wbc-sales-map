8156ce30f35bd29fc49b8bfd6fd68181c47c2973
Trace <tracedelange@me.com>
2021-10-28 13:52:45 -0500

    Typo

--- src/firebase.js
+++ src/firebase.js
@@ -9,7 +9,7 @@ const projectId = process.env.REACT_APP_PROJECTID
 const storageBucket = process.env.REACT_APP_STORAGEBUCKET
 const messagingSenderId = process.env.REACT_APP_MESSAGINGSENDERID
 const appId = process.env.REACT_APP_APPID
-const appCheck = process.env.REACT_APP_APPCHECK
+const appCheckKey = process.env.REACT_APP_APPCHECK
 
 const firebaseConfig = {
     apiKey: apiKey,
@@ -26,7 +26,7 @@ firebase.initializeApp(firebaseConfig);
 const appCheck = firebase.appCheck();
 // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
 // key is the counterpart to the secret key you set in the Firebase console.
-appCheck.activate(appCheck, true);
+appCheck.activate(appCheckKey, true);
 
 // console.log('Connection established')
 const database = firebase.database()
